4a955a826807ed0e43de07e68cc2d9f6e8f1d3ae
Aleksei Viunik <vjunik88@gmail.com>
2025-05-08 12:18:58 +0200

    add new consts

--- consts.py
+++ consts.py
@@ -3,3 +3,26 @@ Here the principal consts are defined.
 """
 
 PRODUCTION_MODE = False
+
+
+EXE_FROZEN = 'frozen'
+
+EMPTY_STRING = ''
+
+STR_CODING = 'utf-8'
+FILE_READ = 'r'
+FILE_WRITE = 'w'
+
+FNF_MESSAGE = 'File not found.'
+INDENT = 4
+SET_TO_ZERO = 0
+SET_TO_ONE = 1
+SETTINGS_JSON_ABS = "settings.json"
+EXCEL_FILEPATH_KEY = "filepath"
+EXCEL_SHEET_NAME = "Foglio1"
+
+EXCEL_START_ROW = 4
+EXCEL_START_COL = 2
+EXCEL_END_COL = 7
+
+TIME_TRESHOLD = 30
