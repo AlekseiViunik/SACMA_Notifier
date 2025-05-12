06d43c75f4780010f85fd0335694e04d76eff52a
Aleksei Viunik <vjunik88@gmail.com>
2025-05-12 09:35:59 +0200

    extend checker's logic

--- logic/checker.py
+++ logic/checker.py
@@ -1,5 +1,6 @@
 from logic.json_handler import JsonHandler
 from logic.excel_handler import ExcelHandler
+from logic.mail_handler import MailHandler
 import consts as c
 
 
@@ -12,4 +13,28 @@ class Checker:
     def check(self):
         self.excel_filepath = self.json.get_value_by_key(c.EXCEL_FILEPATH_KEY)
         self.excel.filepath = self.excel_filepath
-        self.excel.check_file()
+
+        workers_info = self.json.get_value_by_key(c.WORKERS)
+
+        result = {}
+        for worker, info in workers_info.items():
+
+            result[worker] = self.excel.check_file(
+                info[c.START_COLUMN],
+                info[c.END_COLUMN]
+            )
+
+        if result:
+            for name, data in result.items():
+                if not data:
+                    continue
+                mailer = MailHandler()
+                recipient = workers_info[name][c.EMAIL]
+                message = mailer.prepare_message(data)
+                mailer.send_email(
+                    recipient,
+                    "Documenti in scadenza",
+                    message
+                )
+        else:
+            print("No results found.")
