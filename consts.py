d0e8d4169932e52e8a53193ef96511ce30e6b6e1
Aleksei Viunik <vjunik88@gmail.com>
2025-05-12 09:34:38 +0200

    add new consts

--- consts.py
+++ consts.py
@@ -17,12 +17,37 @@ FNF_MESSAGE = 'File not found.'
 INDENT = 4
 SET_TO_ZERO = 0
 SET_TO_ONE = 1
-SETTINGS_JSON_ABS = "settings.json"
-EXCEL_FILEPATH_KEY = "filepath"
-EXCEL_SHEET_NAME = "Foglio1"
 
-EXCEL_START_ROW = 4
-EXCEL_START_COL = 2
-EXCEL_END_COL = 7
+SETTINGS_JSON_ABS = "settings.json"
+
+# Excel consts
+if True:
+    EXCEL_FILEPATH_KEY = "filepath"
+    EXCEL_SHEET_NAME = "Foglio1"
+    EXCEL_START_ROW = 4
+    EXCEL_NAME_COL = 2
+    EXCEL_END_COL = 7
+    HEADER_ROW = 3
 
 TIME_TRESHOLD = 30
+
+# Dict keys
+if True:
+    WORKERS = "workers"
+    EMAIL = "email"
+    START_COLUMN = "start_column"
+    END_COLUMN = "end_column"
+
+# ENV keys
+if True:
+    SMTP_SERVER_KEY = "SMTP_SERVER"
+    SMTP_PORT_KEY = "SMTP_PORT"
+    SENDER_ADDRESS_KEY = "EMAIL_ADDRESS"
+    EMAIL_PASSWORD_KEY = "EMAIL_PASSWORD"
+    RECIPIENT_KEY = "DEFAULT_RECIPIENT"
+
+# Mail consts
+if True:
+    SUBJECT = "Subject"
+    FROM = "From"
+    TO = "To"
