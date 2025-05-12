0b3c53c0ed94baaab200eb943332d4513d43cd96
Aleksei Viunik <vjunik88@gmail.com>
2025-05-12 11:15:19 +0200

    add logging to the main file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from logic.checker import Checker
+from logic.logger import logger as lg
 
 if __name__ == "__main__":
+
+    lg.info("================================================================")
+    lg.info("Starting the script...")
     checker = Checker()
     checker.check()
+    lg.info("Script finished.")
+    lg.info("================================================================")
