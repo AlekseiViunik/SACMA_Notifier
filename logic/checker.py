8674fef2d5702eebb8860875e837f96ea3b65622
Aleksei Viunik <vjunik88@gmail.com>
2025-05-12 11:15:30 +0200

    add logging to the checker

--- logic/checker.py
+++ logic/checker.py
@@ -1,5 +1,6 @@
 from logic.json_handler import JsonHandler
 from logic.excel_handler import ExcelHandler
+from logic.logger import logger as lg
 from logic.mail_handler import MailHandler
 import consts as c
 
@@ -11,25 +12,63 @@ class Checker:
         self.excel_filepath: str = c.EMPTY_STRING
 
     def check(self):
-        self.excel_filepath = self.json.get_value_by_key(c.EXCEL_FILEPATH_KEY)
+        lg.info("Start checking...")
+        lg.info("Get excel file path from settings json file.")
+        try:
+            self.excel_filepath = self.json.get_value_by_key(
+                c.EXCEL_FILEPATH_KEY
+            )
+            lg.info(f"Excel file path: {self.excel_filepath}")
+
+        except KeyError as e:
+            lg.error(f"Key not found in JSON file: {e}.")
+            return
+        except Exception as e:
+            lg.error(f"An error occurred: {e}.")
+            return
+
         self.excel.filepath = self.excel_filepath
 
-        workers_info = self.json.get_value_by_key(c.WORKERS)
+        lg.info("Get the necessary info about responsible workers.")
+        try:
+            workers_info = self.json.get_value_by_key(c.WORKERS)
+            workers = [item for item in workers_info.keys()]
+            lg.info(f"Success! Responsible workers: {workers}.")
+        except KeyError as e:
+            lg.error(f"Key '{c.WORKERS}' not found in JSON file: {e}.")
+            return
+        except Exception as e:
+            lg.error(f"An error occurred: {e}.")
+            return
 
         result = {}
         for worker, info in workers_info.items():
-
+            lg.info(
+                f"+++++++=======Check information for {worker}.=======+++++++"
+            )
             result[worker] = self.excel.check_file(
                 info[c.START_COLUMN],
                 info[c.END_COLUMN]
             )
+        lg.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        lg.info("Finished checking the file for all responsible workers.")
+        lg.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
         if result:
+            lg.info("There are expired dates!")
             for name, data in result.items():
+                lg.info(
+                    f"+++++++=======Prepare data for {name}.=======+++++++"
+                )
                 if not data:
+                    lg.info(f"No expired dates for {name}.")
                     continue
+
                 mailer = MailHandler()
+
                 recipient = workers_info[name][c.EMAIL]
+                lg.info(f"Set {name}'s email as '{recipient}'.")
+                lg.info(f"Prepare message for {name}.")
                 message = mailer.prepare_message(data)
                 mailer.send_email(
                     recipient,
@@ -37,4 +76,5 @@ class Checker:
                     message
                 )
         else:
+            lg.info("No expired dates found.")
             print("No results found.")
